ede30b400fea54fd004210c17651ae13fa0b6d79
Usman Kiani <usmankiani256@gmail.com>
2021-08-18 18:31:01 +0500

    update state to set interval

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,21 +2,46 @@ import * as React from 'react'
 import { ProgressBar as PaperProgressBar } from 'react-native-paper'
 
 const ProgressBar = (props) => {
-  const { progress, color, loop, visible } = props
+  const { progress, color, loop, visible, time } = props
+
+  const [prog, setProg] = React.useState(0)
+
+  React.useEffect(() => {
+    let mul = 1
+
+    if (time) {
+      let interval = setInterval(() => {
+        let rate = 1 / time
+        let updated = rate * mul
+
+        console.log('Progress: ' + parseInt(updated * 100) + '%')
+
+        setProg(updated)
+        mul++
+
+        if (updated >= 1) {
+          console.log('Removing interval with id: ', interval)
+          clearInterval(interval)
+        }
+      }, 1000)
+    }
+  }, [])
 
   let _progress = progress || 0.5
   let _color = color || 'teal'
   let _indeterminate = loop || false
   let _visible = visible || true
+  let _time = time || null
 
   return (
     <PaperProgressBar
-      progress={_progress}
+      progress={time ? prog : _progress}
       color={_color}
       indeterminate={_indeterminate}
       visible={_visible}
+      time={_time}
       {...props}
-    ></PaperProgressBar>
+    />
   )
 }
 
