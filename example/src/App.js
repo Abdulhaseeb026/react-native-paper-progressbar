c0964b304d909e24777b9556d5f6537f8298e51d
Usman Kiani <usmankiani256@gmail.com>
2021-08-18 18:32:05 +0500

    update app.js

---
+++ example/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View } from 'react-native'
+import ProgressBar from 'react-native-paper-progressbar'
+
+const App = () => {
+  return (
+    <View style={styles.root}>
+      <ProgressBar time={20} />
+    </View>
+  )
+}
+
+const styles = {
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 18,
+  },
+}
+
+export default App
